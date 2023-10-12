3d7b3dd2004698e6a1c817ded9e8e97be9399044
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-10-13 00:18:24 +0600

    practicing framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Navbar from "./Component/Shared/Navbar/Navbar";
+import Test from "./Component/Test/Test";
 import "./app.scss"
 const App = () => {
   return <div className="app">
-    <section><Navbar></Navbar></section>
+   {/*  <section><Navbar></Navbar></section>
     <section>paralax</section>
     <section>Service</section>
     <section>Paralax</section>
     <section>Portfulio</section>
-    <section>Contact</section>
+    <section>Contact</section> */} 
+    <Test></Test>
+    <Test></Test>
   </div>;
 };
 
---
+++ src/Component/Test/Test.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./Test.scss"
+import { motion } from "framer-motion"
+const Test = () => {
+
+    const [open, setOpen] = useState(false)
+    const variants = {
+        visible: { opacity: 1 },
+        hidden: { opacity: 0 }
+    }
+    return (
+        <div className="course">
+            <motion.div className="box" variants={variants} transition={{ duration: 2 }} animate={open ? "visible" : "hidden"}>
+            </motion.div>
+            <button className="text-white text-xl" onClick={() => setOpen(prev => !prev)}>click here</button>
+        </div>
+    );
+};
+
+export default Test;
